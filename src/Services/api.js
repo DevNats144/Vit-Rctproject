889615bf260e893cdd0bb8389a47760e5fc81e40
DevNats144 <Natanielg73@gmail.com>
2025-08-14 12:18:15 +0100

    Alterei da Url local, para a remota

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
 
-    baseURL: 'http://localhost:4000'//Endereco onde se encontra a nossa API
+    baseURL: 'https://apiproj-production.up.railway.app'//Endereco onde se encontra a nossa API
 
 })
 
