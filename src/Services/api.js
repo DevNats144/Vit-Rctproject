62afb2d8b0e2c4e28d0aa3fbe68aaf3a62a730e6
DevNats144 <Natanielg73@gmail.com>
2025-09-18 19:10:38 +0100

    De volta pro Render

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Api = axios.create({
 
-    baseURL: 'https://apiproj-production.up.railway.app'//Endereco onde se encontra a nossa API
+    baseURL: 'https://apiproj-render.onrender.com'//Endereco onde se encontra a nossa API
 
 })
 
